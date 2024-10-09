25f15fb36499be54c69211ee6fbd48f3b26d10bf
Julian Wixson <julian@adobe.com>
2024-10-09 11:36:37 -0700

    Removed Support link from top navigation bar and related sub-pages from the Gatsby config. Also removed support page files from the repo.

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,10 +31,6 @@ module.exports = {
             path: '/api/current/index.md'
           },
         ]
-      },
-      {
-        title: 'Support',
-        path: '/support/'
       }
     ],
     subPages: [
@@ -59,36 +55,6 @@ module.exports = {
       {
         title: 'Migrating',
         path: '/guides/migrating/'
-      },
-      {
-        title: 'Overview',
-        path: '/support/',
-        header: true,
-        pages: [
-          {
-            title: 'Help',
-            path: '/support/'
-          },
-          {
-            title: 'FAQ',
-            path: '/support/FAQ/'
-          },
-          {
-            title: 'How to contribute',
-            path: '/support/contribute/'
-          }
-        ]
-      },
-      {
-        title: 'Community',
-        path: '/support/community/',
-        header: true,
-        pages: [
-          {
-            title: 'Information',
-            path: '/support/community/'
-          }
-        ]
       }
     ]
   },
